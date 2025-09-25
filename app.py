415145b706474f417088637eb654a3ab14cf6a5c
tasueom <tasueom@naver.com>
2025-09-25 14:42:11 +0900

    feat(db): 오라클db 패키지 임포트 및 데이터베이스 연결

---
+++ app.py
@@ -0,0 +1,16 @@
+import oracledb
+
+oracledb.init_oracle_client(
+    lib_dir=r"C:\oraclexe\instantclient-basic-windows.x64-23.9.0.25.07\instantclient_23_9"
+)
+
+def conn_db():
+    conn = oracledb.connect(
+    user="hr",
+    password="1234",
+    dsn="localhost:1521/XE"
+    )
+        
+    cur = conn.cursor()
+    
+    return conn, cur
